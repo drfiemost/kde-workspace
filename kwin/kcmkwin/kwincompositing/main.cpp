drfiemost/kde-workspace

commit 243e5949d251c47c2ca7a9a3044089eec806b754

Make KNS3 optional (fixes #2)

kwin/kcmkwin/kwincompositing/main.cpp
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <kservice.h>
 #include <ktitlewidget.h>
 #include <knotification.h>
-#include <KNS3/DownloadDialog>
+//#include <KNS3/DownloadDialog>
 
 #include <QtDBus/QtDBus>
 #include <QPainter>
@@ -124,8 +124,9 @@ KWinCompositingConfig::KWinCompositingConfig(QWidget *parent, const QVariantList
         warn(message, details, dontAgainKey);
     } else
         ui.messageBox->setVisible(false);
+#ifdef ENABLE_KNEWSTUFF3
     ui.ghns->setIcon(KIcon("get-hot-new-stuff"));
-
+#endif
     // For future use
     (void) I18N_NOOP("Use GLSL shaders");
 
@@ -179,8 +180,9 @@ KWinCompositingConfig::KWinCompositingConfig(QWidget *parent, const QVariantList
     connect(ui.glColorCorrection, SIGNAL(toggled(bool)), this, SLOT(changed()));
     connect(m_showDetailedErrors, SIGNAL(triggered(bool)), SLOT(showDetailedEffectLoadingInformation()));
     connect(m_dontShowAgain, SIGNAL(triggered(bool)), SLOT(blockFutureWarnings()));
+#ifdef ENABLE_KNEWSTUFF3
     connect(ui.ghns, SIGNAL(clicked(bool)), SLOT(slotGHNS()));
-
+#endif
     // Open the temporary config file
     // Temporary conf file is used to synchronize effect checkboxes with effect
     // selector by loading/saving effects from/to temp config when active tab
@@ -898,7 +900,7 @@ QString KWinCompositingConfig::quickHelp() const
 {
     return i18n("<h1>Desktop Effects</h1>");
 }
-
+#ifdef ENABLE_KNEWSTUFF3
 void KWinCompositingConfig::slotGHNS()
 {
     QPointer<KNS3::DownloadDialog> downloadDialog = new KNS3::DownloadDialog("kwineffect.knsrc", this);
@@ -909,7 +911,7 @@ void KWinCompositingConfig::slotGHNS()
     }
     delete downloadDialog;
 }
-
+#endif
 } // namespace
 
 #include "dbus.moc"

plasma/desktop/shell/CMakeLists.txt
@@ -63,7 +63,11 @@ if(UNIX AND Q_WS_X11)
 endif(UNIX AND Q_WS_X11)
 
 target_link_libraries(kdeinit_plasma-desktop kephal)
-target_link_libraries(kdeinit_plasma-desktop ${KDE4_KNEWSTUFF3_LIBS})
+target_link_libraries(kdeinit_plasma-desktop)
+if(LIBATTICA_FOUND)
+    add_definitions(-DENABLE_KNEWSTUFF3)
+    target_link_libraries(kdeinit_plasma ${KDE4_KNEWSTUFF3_LIBS})
+endif(LIBATTICA_FOUND)
 
 install(TARGETS kdeinit_plasma-desktop DESTINATION ${LIB_INSTALL_DIR})
 install(TARGETS plasma-desktop ${INSTALL_TARGETS_DEFAULT_ARGS})

plasma/generic/wallpapers/image/image.h
@@ -57,7 +57,9 @@ class Image : public Plasma::Wallpaper
         void positioningChanged(int index);
         void addDir();
         void removeDir();
+#ifdef ENABLE_KNEWSTUFF3
         void getNewWallpaper();
+#endif
         void colorChanged(const QColor& color);
         void pictureChanged(const QModelIndex &);
         void wallpaperBrowseCompleted();
@@ -78,7 +80,7 @@ class Image : public Plasma::Wallpaper
         void setWallpaper(const QString &path);
         void setWallpaperRetrieved(KJob *job);
         void addWallpaperRetrieved(KJob *job);
-        void newStuffFinished();
+        //void newStuffFinished();
         void setConfigurationInterfaceModel();
         void updateDirs();
         void updateDirWatch(const QStringList &newDirs);

CMakeLists.txt
@@ -171,6 +171,13 @@ set_package_properties(QJSON PROPERTIES DESCRIPTION "Library to manage JSON obje
                        PURPOSE "Required to build Chrome/Chromium support for Plasma Bookmarks Runners"
                       )
 
+find_package(LibAttica 0.1.4)
+set_package_properties(LibAttica PROPERTIES DESCRIPTION "A library to access Open Collaboration Service providers"
+                       URL "https://projects.kde.org/attica"
+                       TYPE OPTIONAL
+                       PURPOSE "Attica is needed for the Get Hot New Stuff support"
+                      )
+
 
 include(ConfigureChecks.cmake)
 if(NOT WIN32)

kcontrol/colors/CMakeLists.txt
@@ -5,7 +5,11 @@ set(kcm_colors_PART_SRCS ../krdb/krdb.cpp colorscm.cpp previewwidget.cpp setprev
 kde4_add_ui_files(kcm_colors_PART_SRCS colorsettings.ui preview.ui setpreview.ui)
 
 kde4_add_plugin(kcm_colors ${kcm_colors_PART_SRCS})
-target_link_libraries(kcm_colors ${KDE4_KIO_LIBS} ${KDE4_KNEWSTUFF3_LIBS} ${KDE4_KDEUI_LIBS} ${X11_LIBRARIES})
+target_link_libraries(kcm_colors ${KDE4_KIO_LIBS} ${KDE4_KDEUI_LIBS} ${X11_LIBRARIES})
+if(LIBATTICA_FOUND)
+    add_definitions(-DENABLE_KNEWSTUFF3)
+    target_link_libraries(kcm_colors ${KDE4_KNEWSTUFF3_LIBS})
+endif(LIBATTICA_FOUND)
 
 install(TARGETS kcm_colors  DESTINATION ${PLUGIN_INSTALL_DIR})
 install( FILES colors.desktop  DESTINATION  ${SERVICES_INSTALL_DIR} )

kcontrol/desktoptheme/CMakeLists.txt
@@ -6,8 +6,12 @@ kde4_add_ui_files(kcmdesktoptheme_SRCS DesktopTheme.ui DesktopThemeDetails.ui)
 
 kde4_add_plugin(kcm_desktoptheme ${kcmdesktoptheme_SRCS})
 target_link_libraries(kcm_desktoptheme ${X11_LIBRARIES}
-    ${KDE4_KDEUI_LIBS} ${KDE4_KPARTS_LIBS} ${KDE4_KNEWSTUFF3_LIBS}
+    ${KDE4_KDEUI_LIBS} ${KDE4_KPARTS_LIBS}
     ${KDE4_KCMUTILS_LIBRARY} ${KDE4_PLASMA_LIBS} ${KDE4_KIO_LIBS})
+if(LIBATTICA_FOUND)
+    add_definitions(-DENABLE_KNEWSTUFF3)
+    target_link_libraries(kcm_desktoptheme ${KDE4_KNEWSTUFF3_LIBS})
+endif(LIBATTICA_FOUND)
 
 install(TARGETS kcm_desktoptheme DESTINATION ${PLUGIN_INSTALL_DIR} )
 

kcontrol/input/CMakeLists.txt
@@ -86,7 +86,11 @@ set(kcm_cursortheme_PART_SRCS kcmcursortheme.cpp ${libnoinst_SRCS})
 kde4_add_plugin(kcm_cursortheme ${kcm_cursortheme_PART_SRCS})
 
 
-target_link_libraries(kcm_cursortheme ${KDE4_KIO_LIBS} ${KDE4_KNEWSTUFF3_LIBS} ${X11_LIBRARIES})
+target_link_libraries(kcm_cursortheme ${KDE4_KIO_LIBS} ${X11_LIBRARIES})
+if(LIBATTICA_FOUND)
+    add_definitions(-DENABLE_KNEWSTUFF3)
+    target_link_libraries(kcm_cursortheme ${KDE4_KNEWSTUFF3_LIBS})
+endif(LIBATTICA_FOUND)
 
 if (X11_Xcursor_FOUND)
    target_link_libraries(kcm_cursortheme ${X11_Xcursor_LIB})

kcontrol/kfontinst/kcmfontinst/CMakeLists.txt
@@ -9,7 +9,11 @@ set(kcm_fontinst_PART_SRCS ${libkfontinstview_SRCS} ${libkfontinstjobrunner_SRCS
 
 kde4_add_plugin(kcm_fontinst ${kcm_fontinst_PART_SRCS})
 
-target_link_libraries(kcm_fontinst ${KDE4_KIO_LIBS} ${KDE4_KDESU_LIBS} ${KDE4_KNEWSTUFF3_LIBRARY} kfontinstui kfontinst ${X11_LIBRARIES})
+target_link_libraries(kcm_fontinst ${KDE4_KIO_LIBS} ${KDE4_KDESU_LIBS} kfontinstui kfontinst ${X11_LIBRARIES})
+if(LIBATTICA_FOUND)
+    add_definitions(-DENABLE_KNEWSTUFF3)
+    target_link_libraries(kcm_fontinst ${KDE4_KNEWSTUFF3_LIBS})
+endif(LIBATTICA_FOUND)
 
 install(TARGETS kcm_fontinst  DESTINATION ${PLUGIN_INSTALL_DIR} )
 install( FILES fontinst.desktop  DESTINATION  ${SERVICES_INSTALL_DIR} )

commit 9a443ef2258bcdd2878ebaa8ed94cf2ca56cb389

Fix some errors introduced in 243e594

kwin/kcmkwin/kwincompositing/main.cpp
@@ -38,7 +38,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <kservice.h>
 #include <ktitlewidget.h>
 #include <knotification.h>
-//#include <KNS3/DownloadDialog>
+#ifdef ENABLE_KNEWSTUFF3
+#include <KNS3/DownloadDialog>
+#endif
 
 #include <QtDBus/QtDBus>
 #include <QPainter>

plasma/desktop/shell/CMakeLists.txt
@@ -66,7 +66,7 @@ target_link_libraries(kdeinit_plasma-desktop kephal)
 target_link_libraries(kdeinit_plasma-desktop)
 if(LIBATTICA_FOUND)
     add_definitions(-DENABLE_KNEWSTUFF3)
-    target_link_libraries(kdeinit_plasma ${KDE4_KNEWSTUFF3_LIBS})
+    target_link_libraries(kdeinit_plasma-desktop ${KDE4_KNEWSTUFF3_LIBS})
 endif(LIBATTICA_FOUND)
 
 install(TARGETS kdeinit_plasma-desktop DESTINATION ${LIB_INSTALL_DIR})

plasma/generic/wallpapers/image/image.h
@@ -80,7 +80,9 @@ class Image : public Plasma::Wallpaper
         void setWallpaper(const QString &path);
         void setWallpaperRetrieved(KJob *job);
         void addWallpaperRetrieved(KJob *job);
-        //void newStuffFinished();
+#ifdef ENABLE_KNEWSTUFF3
+        void newStuffFinished();
+#endif
         void setConfigurationInterfaceModel();
         void updateDirs();
         void updateDirWatch(const QStringList &newDirs);