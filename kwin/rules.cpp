1bd161b8f402fccf5b88784c0f74014ed1f3f86d
Leandro Nini <drfiemost@users.noreply.github.com>
2023-08-12 18:09:44 +0200

    Comment out useless code
    Fixes a couple of enum-compare warnings

--- kwin/rules.cpp
+++ kwin/rules.cpp
@@ -520,12 +520,12 @@ bool Rules::update(Client* c, int selection)
         updated = updated || noborder != c->noBorder();
         noborder = c->noBorder();
     }
-    if NOW_REMEMBER(OpacityActive, opacityactive) {
+    /*if NOW_REMEMBER(OpacityActive, opacityactive) {
         // TODO
     }
     if NOW_REMEMBER(OpacityInactive, opacityinactive) {
         // TODO
-    }
+    }*/
     return updated;
 }
 
